518ebe9692613369429b9860b229cfaf4604cd04
Mackenzie Jorgensen <mjorgen1@villanova.edu>
2019-06-24 14:28:51 +0200

    finished the deletion of all usernames for training and test datasets

--- dataExtraction/CleanGermData.py
+++ dataExtraction/CleanGermData.py
@@ -1,30 +1,38 @@
 # deletes all the usernames from the original text file and saves the comments and labels to a new text file
+# the new text file has an empty line at the end, so delete if you'd like
 
 def main():
-    # opens the text file
-    f1 = open('germeval2018.training.txt','r')
+    # opens the text file to extract data from, and this needs to be in the same folder as this python script
+    f1 = open('germeval2018.test.txt','r')
 
     # make a new text file and open it
-    f2 = open("cleanGermanDataTraining.txt","w+")
+    f2 = open("cleanGermanDataTest.txt","w+")
 
     # i is for the purposes of testing
     i = 1
 
+    list_spaces = [" ", "  ", "   ", "    ", "     ", "      ", "       ", "        "]
+
     # iterates through the text file line by line
     for line in f1.readlines():
-        # TODO: go through the line looking for @ sign and save to a str until a space is encountered with a non@ following
         # ^ for the above we know the total num of chars in the string
-        # TODO: delete the word(s?)
-        # these should be within a for loop going through the line and it should have a break statement
-        #word = "@"
-       # line = line.replace(word, "")
-
-        f2.write("open")
-
-        # for the purposes of testing below should do one iteration
+        delete = ""
+        if(line.find('@') != -1):
+            for char in line:
+                if(char == "@"):
+                    delete += char
+                    print(char)
+                    start = line.find(char)+1
+                    for x in range(start, len(line)):
+                        if(not (line[x] in list_spaces)):
+                            delete += line[x]
+                        else:
+                            line = line.replace(delete, "").lstrip().rstrip()
+                            delete = ""
+                            break
         i += 1
-        if i == 2:
-            break
+        print(i)
+        f2.write(line)
 
     f1.close()
     f2.close()
