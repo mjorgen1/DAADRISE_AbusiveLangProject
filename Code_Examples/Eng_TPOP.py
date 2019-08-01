cff42c43f618afd7a42f9dca87f9e9b7b669d573
Paspachu <minhochoi.official@gmail.com>
2019-08-01 12:39:35 +0200

    Updated TPOT

--- Code_Examples/Eng_TPOP.py
+++ Code_Examples/Eng_TPOP.py
@@ -210,6 +210,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_
 
 from tpot import TPOTClassifier
 
-tpot = TPOTClassifier(generations=1, population_size=20, verbosity=2, scoring='f1_weighted')
+tpot = TPOTClassifier(generations=1, population_size=20, verbosity=2, scoring='f1_weighted', max_time_mins=60)
 tpot.fit(X_train, y_train)
 print(tpot.score(X_test, y_test))
