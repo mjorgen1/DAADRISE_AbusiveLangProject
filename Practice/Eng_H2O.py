7712acf51c8069c321b8ea08785dc4ec2f28cc80
Paspachu <minhochoi.official@gmail.com>
2019-08-02 10:14:51 +0200

    Updated H2O

---
+++ Practice/Eng_H2O.py
@@ -0,0 +1,294 @@
+import pandas as pd
+import numpy as np
+import pickle
+import sys
+from sklearn.feature_extraction.text import TfidfVectorizer
+import nltk
+from nltk.stem.porter import *
+import string
+import re
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer as VS
+from textstat.textstat import *
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import classification_report
+from sklearn.svm import LinearSVC
+import matplotlib.pyplot as plt
+import seaborn
+
+df = pd.read_csv("labeled_data.csv")
+
+tweets=df.tweet
+
+stopwords = nltk.corpus.stopwords.words("english")
+
+other_exclusions = ["#ff", "ff", "rt"]
+stopwords.extend(other_exclusions)
+
+stemmer = PorterStemmer()
+
+def preprocess(text_string):
+    """
+    Accepts a text string and replaces:
+    1) urls with URLHERE
+    2) lots of whitespace with one instance
+    3) mentions with MENTIONHERE
+
+    This allows us to get standardized counts of urls and mentions
+    Without caring about specific people mentioned
+    """
+    space_pattern = '\s+'
+    giant_url_regex = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'
+        '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    mention_regex = '@[\w\-]+'
+    parsed_text = re.sub(space_pattern, ' ', text_string)
+    parsed_text = re.sub(giant_url_regex, '', parsed_text)
+    parsed_text = re.sub(mention_regex, '', parsed_text)
+    return parsed_text
+
+def tokenize(tweet):
+    """Removes punctuation & excess whitespace, sets to lowercase,
+    and stems tweets. Returns a list of stemmed tokens."""
+    tweet = " ".join(re.split("[^a-zA-Z]+", tweet.lower())).strip()
+    tokens = [stemmer.stem(t) for t in tweet.split()]
+    return tokens
+
+def basic_tokenize(tweet):
+    """Same as tokenize but without the stemming"""
+    tweet = " ".join(re.split("[^a-zA-Z.,!?]+", tweet.lower())).strip()
+    return tweet.split()
+
+vectorizer = TfidfVectorizer(
+    tokenizer=tokenize,
+    preprocessor=preprocess,
+    ngram_range=(1, 3),
+    stop_words=stopwords,
+    use_idf=True,
+    smooth_idf=False,
+    norm=None,
+    decode_error='replace',
+    max_features=10000,
+    min_df=5,
+    max_df=0.75
+    )
+
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+#Construct tfidf matrix and get relevant scores
+tfidf = vectorizer.fit_transform(tweets).toarray()
+vocab = {v:i for i, v in enumerate(vectorizer.get_feature_names())}
+idf_vals = vectorizer.idf_
+idf_dict = {i:idf_vals[i] for i in vocab.values()} #keys are indices; values are IDF scores
+
+#Get POS tags for tweets and save as a string
+tweet_tags = []
+for t in tweets:
+    tokens = basic_tokenize(preprocess(t))
+    tags = nltk.pos_tag(tokens)
+    tag_list = [x[1] for x in tags]
+    tag_str = " ".join(tag_list)
+    tweet_tags.append(tag_str)
+
+#We can use the TFIDF vectorizer to get a token matrix for the POS tags
+pos_vectorizer = TfidfVectorizer(
+    tokenizer=None,
+    lowercase=False,
+    preprocessor=None,
+    ngram_range=(1, 3),
+    stop_words=None,
+    use_idf=False,
+    smooth_idf=False,
+    norm=None,
+    decode_error='replace',
+    max_features=5000,
+    min_df=5,
+    max_df=0.75,
+    )
+
+#Construct POS TF matrix and get vocab dict
+pos = pos_vectorizer.fit_transform(pd.Series(tweet_tags)).toarray()
+pos_vocab = {v:i for i, v in enumerate(pos_vectorizer.get_feature_names())}
+
+# Now get other features
+sentiment_analyzer = VS()
+
+
+def count_twitter_objs(text_string):
+    """
+    Accepts a text string and replaces:
+    1) urls with URLHERE
+    2) lots of whitespace with one instance
+    3) mentions with MENTIONHERE
+    4) hashtags with HASHTAGHERE
+
+    This allows us to get standardized counts of urls and mentions
+    Without caring about specific people mentioned.
+
+    Returns counts of urls, mentions, and hashtags.
+    """
+    space_pattern = '\s+'
+    giant_url_regex = ('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|'
+                       '[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    mention_regex = '@[\w\-]+'
+    hashtag_regex = '#[\w\-]+'
+    parsed_text = re.sub(space_pattern, ' ', text_string)
+    parsed_text = re.sub(giant_url_regex, 'URLHERE', parsed_text)
+    parsed_text = re.sub(mention_regex, 'MENTIONHERE', parsed_text)
+    parsed_text = re.sub(hashtag_regex, 'HASHTAGHERE', parsed_text)
+    return (parsed_text.count('URLHERE'), parsed_text.count('MENTIONHERE'), parsed_text.count('HASHTAGHERE'))
+
+
+def other_features(tweet):
+    """This function takes a string and returns a list of features.
+    These include Sentiment scores, Text and Readability scores,
+    as well as Twitter specific features"""
+    sentiment = sentiment_analyzer.polarity_scores(tweet)
+
+    words = preprocess(tweet)  # Get text only
+
+    syllables = textstat.syllable_count(words)
+    num_chars = sum(len(w) for w in words)
+    num_chars_total = len(tweet)
+    num_terms = len(tweet.split())
+    num_words = len(words.split())
+    avg_syl = round(float((syllables + 0.001)) / float(num_words + 0.001), 4)
+    num_unique_terms = len(set(words.split()))
+
+    ###Modified FK grade, where avg words per sentence is just num words/1
+    FKRA = round(float(0.39 * float(num_words) / 1.0) + float(11.8 * avg_syl) - 15.59, 1)
+    ##Modified FRE score, where sentence fixed to 1
+    FRE = round(206.835 - 1.015 * (float(num_words) / 1.0) - (84.6 * float(avg_syl)), 2)
+
+    twitter_objs = count_twitter_objs(tweet)
+    retweet = 0
+    if "rt" in words:
+        retweet = 1
+    features = [FKRA, FRE, syllables, avg_syl, num_chars, num_chars_total, num_terms, num_words,
+                num_unique_terms, sentiment['neg'], sentiment['pos'], sentiment['neu'], sentiment['compound'],
+                twitter_objs[2], twitter_objs[1],
+                twitter_objs[0], retweet]
+    # features = pandas.DataFrame(features)
+    return features
+
+
+def get_feature_array(tweets):
+    feats = []
+    for t in tweets:
+        feats.append(other_features(t))
+    return np.array(feats)
+
+other_features_names = ["FKRA", "FRE","num_syllables", "avg_syl_per_word", "num_chars", "num_chars_total", \
+                        "num_terms", "num_words", "num_unique_words", "vader neg","vader pos","vader neu", \
+                        "vader compound", "num_hashtags", "num_mentions", "num_urls", "is_retweet"]
+
+feats = get_feature_array(tweets)
+
+#Now join them all up
+M = np.concatenate([tfidf , pos, feats], axis=1)
+
+print(M.shape)
+
+#Finally get a list of variable names
+variables = ['']*len(vocab)
+for k,v in vocab.items():
+    variables[v] = k
+
+pos_variables = ['']*len(pos_vocab)
+for k,v in pos_vocab.items():
+    pos_variables[v] = k
+
+feature_names = variables+pos_variables+other_features_names
+
+X = pd.DataFrame(M)
+y = df['class'].astype(int)
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.1)
+
+from imblearn.under_sampling import ClusterCentroids
+cc = ClusterCentroids(random_state=2)
+X_cc, y_cc = cc.fit_resample(X_train, y_train)
+X_cc = pd.DataFrame(X_cc)
+X_cc.columns = uni_selected_feat['Specs']
+y_cc = pd.DataFrame(y_cc)
+y_cc.columns = ['lablels']
+X_train = X_cc
+y_train = y_cc
+
+y_test = y_test.to_frame(name='labels')
+
+X_train.to_csv('X_train.csv', index=None, header=True, encoding='utf-8')
+X_test.to_csv('X_test.csv', index=None, header=True, encoding='utf-8')
+y_train.to_csv('y_train.csv', index=None, header=True, encoding='utf-8')
+y_test.to_csv('y_test.csv', index=None, header=True, encoding='utf-8')
+
+import h2o
+h2o.init()
+from h2o.automl import H2OAutoML
+X_train = h2o.import_file('X_train.csv')
+y_train = h2o.import_file('y_train.csv')
+X_test = h2o.import_file('X_test.csv')
+y_test = h2o.import_file('y_test.csv')
+# preparing the train and test data sets
+# now convert tweet vecs and labels to a pandas dataframe and back to h2o dataframe
+train = X_train.cbind(y_train)
+test = X_test.cbind(y_test)
+# more on data prep
+x = train.columns         # x: A list/vector of predictor column names or indexes.
+                          # This argument only needs to be specified if the user wants to exclude columns from the
+                          # set of predictors. If all columns (other than the response) should be used in prediction,
+                          # then this does not need to be set.
+
+y = "labels"              # This argument is the name (or index) of the response column
+x.remove(y)
+
+# need to set train and test
+train[y] = train[y].asfactor()
+test[y] = test[y].asfactor()
+# now the AUTO-ML piece comes in
+aml = H2OAutoML(max_runtime_secs=1800, balance_classes=True) #max_models=10 or 20?, max_runtime_secs=3600
+aml.train(x=x, y=y, training_frame=train)
+
+# View the AutoML Leaderboard
+lb = aml.leaderboard
+lb.head(rows=lb.nrows)  # Print all rows instead of default (10 rows)
+# The leader model is stored here
+aml.leader
+
+preds = aml.predict(test)
+print(preds)
+
+var = preds["predict"].cbind(test[y])
+print(var)
+
+# convert to pandas dataframe
+y_test = h2o.as_list(test[y], use_pandas=True)
+y_pred = h2o.as_list(preds["predict"])
+report = classification_report(y_test, y_pred)
+print(report)
+print(metrics.confusion_matrix(y_test, y_pred))
+print(metrics.accuracy_score(y_test, y_pred))
+print(metrics.f1_score(y_test, y_pred, average='weighted'))
+
+confusion_matrix = metrics.confusion_matrix(y_test,y_pred)
+matrix_proportions = np.zeros((3,3))
+for i in range(0,3):
+    matrix_proportions[i,:] = confusion_matrix[i,:]/float(confusion_matrix[i,:].sum())
+names=['Hate','Offensive','Neither']
+confusion_df = pd.DataFrame(matrix_proportions, index=names,columns=names)
+plt.figure(figsize=(5,5))
+seaborn.heatmap(confusion_df,annot=True,annot_kws={"size": 12},cmap='gist_gray_r',cbar=False, square=True,fmt='.2f')
+plt.ylabel(r'True categories',fontsize=14)
+plt.xlabel(r'Predicted categories',fontsize=14)
+plt.tick_params(labelsize=12)
+plt.savefig('C:/Users/mikec/Documents/Results/Univariate3000ClusterMax05.png')
+
+f = open("C:/Users/mikec/Documents/Results/Univariate3000ClusterMax05.txt", "a")
+print("Univariate feature selection with 3000 features and under-sampling with cluster centroids, max run time 30 mins", file=f)
+print(report, file=f)
+print(metrics.confusion_matrix(y_test, y_pred), file=f)
+print(metrics.accuracy_score(y_test, y_pred), file=f)
+print(metrics.f1_score(y_test, y_pred, average='weighted'), file=f)
+f.close()
