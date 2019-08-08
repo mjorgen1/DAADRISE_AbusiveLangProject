39069e09834ff88147af9919d09fde580fa7ba76
Paspachu <minhochoi.official@gmail.com>
2019-08-08 14:55:20 +0200

    Updataed H2O

--- Practice/Eng_H2O.py
+++ Practice/Eng_H2O.py
@@ -202,7 +202,6 @@ X.columns = feature_names
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.1)
 
 X_test.to_csv('X_test.csv', index=None, header=True, encoding='utf-8')
-y_train = y_train.to_frame(name='labels')
 y_test = y_test.to_frame(name='labels')
 y_test.to_csv('y_test.csv', index=None, header=True, encoding='utf-8')
 
